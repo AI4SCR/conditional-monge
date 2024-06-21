2907937a3d33888d68a0db2b53b9774a323155db
Alice Driessen <alice@driessenmarks.nl>
2024-06-21 03:53:36 -0400

    Added CAR code

--- cmonge/datasets/single_loader.py
+++ cmonge/datasets/single_loader.py
@@ -423,4 +423,107 @@ class FourIModule(AbstractDataModule):
         return test_loaders
 
 
-DataModuleFactory = {"4i": FourIModule, "sciplex": SciPlexModule}  # , "synthetic": SyntheticDosageModule}
+class CarModule(AbstractDataModule):
+    def __init__(self, config: DotMap) -> None:
+        super().__init__()
+        self.setup(**config)
+
+    def setup(
+        self,
+        name: str,
+        file_path: Path,
+        drugs_path: Path,
+        features: Path,
+        split: list[float],
+        batch_size: int,
+        drug_col: str,
+        drug_condition: str,
+        control_condition: str,
+        ae: bool,
+        seed: int,
+        ae_config_path: Optional[Path] = None,
+        reduction: Optional[str] = None,
+        parent: Optional[AnnData] = None,
+        parent_reducer: Optional[str] = None,
+    ) -> None:
+        self.name = name
+        self.file_path = file_path
+        self.split = split
+        self.batch_size = batch_size
+        self.features_path = features
+        self.drugs_path = drugs_path
+        self.drug_col = drug_col
+        self.drug_condition = drug_condition
+        self.control_condition = control_condition
+        self.ae = ae
+        self.ae_config = load_config(ae_config_path) if ae_config_path else None
+        self.reduction = reduction
+        self.parent = parent
+        self.parent_reducer = parent_reducer
+        self.seed = seed
+        self.key = jax.random.PRNGKey(self.seed)
+
+        self.loader()
+        self.preprocesser()
+        self.splitter()
+        self.reducer()
+
+
+    def loader(self) -> None:
+        if self.parent:
+            self.adata = self.parent
+        else:
+            self.adata = sc.read_h5ad(self.file_path)
+        with open(self.features_path) as f:
+            features = f.readlines()
+        self.features = [feature.rstrip() for feature in features]
+
+        with open(self.drugs_path) as f:
+            drugs = f.readlines()
+        self.drugs = [drug.rstrip() for drug in drugs]
+
+    def preprocesser(self) -> None:
+        self.adata = self.adata[:, self.features].copy()
+        self.adata.X = self.adata.layers['logcounts']
+        self.adata.X = jnp.asarray(self.adata.X.todense())
+
+
+    def reducer(self):
+        """Sets up dimensionality reduction, either with PCA, AE or identity."""
+        if self.reduction == "pca":
+            self.pca_means = self.adata.X.mean(axis=0)
+            self.encoder = lambda x: (x - self.pca_means) @ self.adata.varm["PCs"]
+            self.decoder = lambda x: x @ self.adata.varm["PCs"].T + self.pca_means
+        elif self.reduction == "ae":
+            if self.parent_reducer:
+                trainer = self.parent_reducer
+            else:
+                trainer = AETrainerModule(self.ae_config)
+                trainer.load_model(self.name, self.drug_condition)
+            model = trainer.model.bind({"params": trainer.state.params})
+            self.encoder = lambda x: model.encoder(x)
+            self.decoder = lambda x: model.decoder(x)
+        else:
+            self.encoder = lambda x: x
+            self.decoder = lambda x: x
+
+    def train_dataloaders(self) -> Tuple[Iterator[jnp.ndarray], Iterator[jnp.ndarray]]:
+        train_loaders = self.get_loaders_by_type("train")
+        return train_loaders
+
+    def valid_dataloaders(self) -> Tuple[Iterator[jnp.ndarray], Iterator[jnp.ndarray]]:
+        valid_loaders = self.get_loaders_by_type("valid")
+        return valid_loaders
+
+    def test_dataloaders(
+        self, batch_size: Optional[int] = None
+    ) -> Tuple[Iterator[jnp.ndarray], Iterator[jnp.ndarray]]:
+        test_loaders = self.get_loaders_by_type("test", batch_size)
+        return test_loaders
+
+
+DataModuleFactory = {
+    "4i": FourIModule,
+    "sciplex": SciPlexModule,
+    "car": CarModule,
+}  # , "synthetic": SyntheticDosageModule}
--- cmonge/models/embedding.py
+++ cmonge/models/embedding.py
@@ -179,4 +179,236 @@ class ModeOfActionEmbedding(BaseEmbedding):
         return condition_batch
 
 
-embed_factory = {"rdkit": RDKitEmbedding, "dose": DoseEmbedding, "moa": ModeOfActionEmbedding}
+class CAR11DimEmbedding(BaseEmbedding):
+    def __init__(
+        self,
+        datamodule: ConditionalDataModule,
+        checkpoint: bool,
+        name: str,
+        model_dir: str,
+        batch_size=None #For compatability
+    ) -> None:
+        super().__init__(datamodule.batch_size)
+        self.model_dir = Path(model_dir)
+        if not checkpoint:
+            labels = datamodule.train_conditions
+            
+            car_11d=pd.DataFrame([self.encode_CAR_11dim(label) for label in labels]).T
+            car_11d.colums = labels
+            self.model_dir.mkdir(parents=True, exist_ok=True)
+            model_dir = self.model_dir / name
+            car_11d.to_csv(model_dir)
+        else:
+            model_dir = self.model_dir / name
+            car_11d = pd.read_csv(model_dir)
+            car_11d = car_11d.drop(columns=["Unnamed: 0"])
+
+        for index, row in car_11d.T.iterrows():
+            values = jnp.asarray(row.values.astype("float"))
+            self.embeddings[index] = values
+
+
+    def encode_CAR_11dim(self, CAR: str)->list:
+        """
+        Compute one-hot encoding of CAR variant on 15 bits.
+        Use alphabetical order of CAR domains: 41BB, CD28, CD40, CTLA4, IL15RA.
+        For each domain there are 3 bits:
+            - Domain present
+            - 1st position
+            - 2nd position
+        So the three bits of CAR with domain A in the first position would be [1,1,0].
+        A in second position would be [1,0,1] and for the CAR with both domains A [1,1,1].
+        The 3 bits for the cars are concatenated into 15 bit. Then the l6th bit is to
+        indicate wether CD3z (`z`) is present.
+        0 everywhere is TCR-
+        """
+        all_domains = ["41BB", "CD28", "CD40", "CTLA4", "IL15RA"]
+        CAR_variant = CAR.split("-")
+
+        encoding = [0] * 11
+
+        if CAR_variant[0] != "NA":
+            # First mark first domain
+            index_1 = all_domains.index(CAR_variant[0])
+            encoding[index_1] = 1
+
+        # Mark second domain if present
+        if CAR_variant[1] != "NA":
+            index_2 = all_domains.index(CAR_variant[1])
+            encoding[index_2 + 5] = 1
+
+        if CAR_variant[2] == "z":
+            encoding[-1] = 1
+
+        return encoding
+
+    def __call__(self, condition: str):
+        condition = self.embeddings[condition]
+        condition_batch = jnp.asarray([condition for _ in range(self.batch_size)])
+        return condition_batch
+
+class CAR16DimEmbedding(BaseEmbedding):
+    def __init__(
+        self,
+        datamodule: ConditionalDataModule,
+        checkpoint: bool,
+        name: str,
+        model_dir: str,
+        batch_size=None #For compatability
+    ) -> None:
+        super().__init__(datamodule.batch_size)
+        self.model_dir = Path(model_dir)
+        if not checkpoint:
+            labels = datamodule.train_conditions
+            
+            car_16d=pd.DataFrame([self.encode_CAR_16dim(label) for label in labels]).T
+            car_16d.colums = labels
+            self.model_dir.mkdir(parents=True, exist_ok=True)
+            model_dir = self.model_dir / name
+            car_16d.to_csv(model_dir)
+        else:
+            model_dir = self.model_dir / name
+            car_16d = pd.read_csv(model_dir)
+            car_16d = car_16d.drop(columns=["Unnamed: 0"])
+
+        for index, row in car_16d.T.iterrows():
+            values = jnp.asarray(row.values.astype("float"))
+            self.embeddings[index] = values
+
+
+    def encode_CAR_16dim(CAR):
+        """
+        Compute one-hot encoding of CAR variant on 16 bits.
+        Use alphabetical order of CAR domains: 41BB, CD28, CD40, CTLA4, IL15RA.
+        For each domain there are 3 bits:
+            - Domain present
+            - 1st position
+            - 2nd position
+        So the three bits of CAR with domain A in the first position would be [1,1,0].
+        A in second position would be [1,0,1] and for the CAR with both domains A [1,1,1].
+        The 3 bits for the cars are concatenated into 15 bit. Then the 16th bit is to
+        indicate wether CD3z (`z`) is present.
+        0 everywhere is TCR-
+        """
+        all_domains = ["41BB", "CD28", "CD40", "CTLA4", "IL15RA"]
+        CAR_variant = CAR.split("-")
+
+        encoding = [0] * 16
+
+        if CAR_variant[0] != "NA":
+            # First mark first domain
+            index_1 = all_domains.index(CAR_variant[0])
+            encoding[index_1 * 3] = 1
+            encoding[index_1 * 3 + 1] = 1
+
+        # Mark second domain if present
+        if CAR_variant[1] != "NA":
+            index_2 = all_domains.index(CAR_variant[1])
+            encoding[index_2 * 3] = 1
+            encoding[index_2 * 3 + 2] = 1
+
+        if CAR_variant[2] == "z":
+            encoding[-1] = 1
+
+        return encoding
+
+    def __call__(self, condition: str):
+        condition = self.embeddings[condition]
+        condition_batch = jnp.asarray([condition for _ in range(self.batch_size)])
+        return condition_batch
+
+
+class CarEsmSmall(BaseEmbedding):
+    def __init__(
+        self,
+        datamodule: ConditionalDataModule,
+        checkpoint: bool,
+        name: str,
+        model_dir: str,
+        batch_size=None #For compatability
+    ) -> None:
+        super().__init__(datamodule.batch_size)
+        self.model_dir = Path(model_dir)
+        if not checkpoint:
+            logger.error("ESM embedding only works with checkpoint, please save a pre-computed embedding")
+        else:
+            model_dir = self.model_dir / name
+            embed = pd.read_csv(model_dir)
+            embed = embed.drop(columns=["Unnamed: 0"])
+
+        for index, row in embed.T.iterrows():
+            values = jnp.asarray(row.values.astype("float"))
+            self.embeddings[index] = values
+
+    def __call__(self, condition: str):
+        condition = self.embeddings[condition]
+        condition_batch = jnp.asarray([condition for _ in range(self.batch_size)])
+        return condition_batch
+
+class MetaDataEmbedding(BaseEmbedding):
+    def __init__(
+        self,
+        datamodule: ConditionalDataModule,
+        checkpoint: bool,
+        name: str,
+        model_dir: str,
+        batch_size=None #For compatability
+    ) -> None:
+        super().__init__(datamodule.batch_size)
+        self.model_dir = Path(model_dir)
+        dataset = datamodule.data_config.file_path.split("/")[-1][:-5]
+        if not checkpoint:
+            adata = datamodule.data_config.parent
+            group="CAR_Variant"
+            cont_scores = ["Cytotoxicity_1", "Proinflamatory_2", "Memory_3", "CD4_Th1_4", "CD4_Th2_5", 'S.Score', 'G2M.Score']
+            fraction_scores = ["Donor", "Time", "Phase", "ident", "subset"]
+            
+            means = adata.obs[[group] +  cont_scores].groupby(group).mean()
+            stds = adata.obs[[group] +  cont_scores].groupby(group).std()
+            cont_features = means.merge(stds, left_index=True, right_index=True, suffixes=("_mean", "_std"))
+
+            group_size = adata.obs.groupby(group).size()
+            all_cat_counts = []
+            for cat in fraction_scores:
+                temp = adata.obs.groupby([group, cat], observed=False).size().reset_index(drop=False)
+                temp = pd.pivot_table(data=temp, index=group, columns=cat, values=0, observed=False)
+                all_cat_counts.append(temp)
+
+            cat_features = pd.concat(all_cat_counts, axis=1)
+            cat_features = cat_features.div(group_size, axis=0)
+
+            embedding = cont_features.merge(cat_features, left_index=True, right_index=True).T
+
+
+            self.model_dir.mkdir(parents=True, exist_ok=True)
+            model_dir = self.model_dir / f"{dataset}_{name}"
+            embedding.to_csv(model_dir)
+        else:
+            model_dir = self.model_dir / f"{dataset}_{name}"
+            embedding = pd.read_csv(model_dir)
+            embedding = embedding.drop(columns=["Unnamed: 0"])
+
+        for index, row in embedding.T.iterrows():
+            values = jnp.asarray(row.values.astype("float"))
+            self.embeddings[index] = values
+
+
+    def __call__(self, condition: str):
+        condition = self.embeddings[condition]
+        condition_batch = jnp.asarray([condition for _ in range(self.batch_size)])
+        return condition_batch
+
+
+embed_factory = {
+    "rdkit": RDKitEmbedding,
+    "dose": DoseEmbedding,
+    "moa": ModeOfActionEmbedding,
+    "embed_11d": CAR11DimEmbedding,
+    "embed_16d": CAR16DimEmbedding,
+    "esm_small": CarEsmSmall,
+    "esm_small_full_dim": CarEsmSmall,
+    "esm_small_full_seq": CarEsmSmall,
+    "esm_small_tail_dim": CarEsmSmall,
+    "esm_small_tail_seq": CarEsmSmall,
+    "metadata": MetaDataEmbedding,
+}
