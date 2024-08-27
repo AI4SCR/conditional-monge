fab41d0b856c2b6a4f7f4e8e8d69d7039751e0df
Alice Driessen <alice@driessenmarks.nl>
2024-08-27 04:15:00 -0400

    fix typo and added .get() function

--- cmonge/trainers/conditional_monge_trainer.py
+++ cmonge/trainers/conditional_monge_trainer.py
@@ -55,17 +55,20 @@ class ConditionalMongeTrainer(AbstractTrainer):
 
         # setup optimizer and scheduler
         opt_fn = optim_factory[self.config.optim.name]
-        if self.config.lr_scheduler.name.lower() == "cosine":
+        lr_scheduler = self.config.get("lr_scheduler", DotMap({"name": "cosine"}))
+        if lr_scheduler.name.lower() == "cosine":
+            print("Setting up cosine optimizer")
             lr_scheduler = optax.cosine_decay_schedule(
                 init_value=self.config.optim.lr,
                 decay_steps=self.num_train_iters,
                 alpha=1e-2,
             )
-        elif self.config.lr_scheduler.name.lower() == "linear":
+        elif lr_scheduler.name.lower() == "linear":
+            print("Setting up linear optimizer")
             lr_scheduler = optax.linear_onecycle_schedule(
                 transition_steps=self.num_train_iters,
                 peak_value=self.config.optim.lr,
-                **self.config.lr_scheduler.kwags,
+                **lr_scheduler.kwargs,
             )
         optimizer = opt_fn(learning_rate=lr_scheduler, **self.config.optim.kwargs)
 
