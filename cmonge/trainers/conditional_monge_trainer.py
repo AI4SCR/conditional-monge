f2d64404a4475c8e5d860b71fcabfa97d0b37363
Alice Driessen <alice@driessenmarks.nl>
2024-10-20 06:19:32 -0400

    Small changes to run 4i combi treatments, using dose_split in setup

--- cmonge/trainers/conditional_monge_trainer.py
+++ cmonge/trainers/conditional_monge_trainer.py
@@ -43,9 +43,6 @@ class ConditionalMongeTrainer(AbstractTrainer):
         self.regularizer_strength = 1
         self.num_train_iters = self.config.num_train_iters
         self.grad_acc_steps = self.config.optim.get("grad_acc_steps", 1)
-
-        self.dose_split = self.config.embedding.get("dose_split", True)
-
         self.init_model(datamodule=datamodule)
 
     def init_model(self, datamodule: ConditionalDataModule):
@@ -94,9 +91,7 @@ class ConditionalMongeTrainer(AbstractTrainer):
         condition_to_loaders = datamodule.get_loaders_by_type(split_type)
         condition = datamodule.sample_condition(split_type)
         loader_source, loader_target = condition_to_loaders[condition]
-        embeddings, n_contexts = self.embedding_module(
-            condition=condition, dose_split=self.dose_split
-        )
+        embeddings, n_contexts = self.embedding_module(condition=condition)
         return (
             {
                 "source": next(loader_source),
@@ -289,9 +284,7 @@ class ConditionalMongeTrainer(AbstractTrainer):
             self.metrics[split_type] = {}
             for cond, loader in cond_to_loaders.items():
                 logger.info(f"Evaluation started on {cond} {split_type}.")
-                cond_embedding, n_contexts = self.embedding_module(
-                    cond, self.dose_split
-                )
+                cond_embedding, n_contexts = self.embedding_module(cond)
                 loader_source, loader_target = loader
 
                 self.metrics[split_type][cond] = {}
